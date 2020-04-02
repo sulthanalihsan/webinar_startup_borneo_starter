589c4b7bfcb915a9070be75b610346b43b1e778e
Muhammad Sulthan Al Ihsan <sulthanalihsan5@gmail.com>
2020-04-02 23:46:35 +0800

    first

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:webinarstartupborneostarter/common/color_palettes.dart';
+import 'package:webinarstartupborneostarter/common/image_asset.dart';
+import 'package:webinarstartupborneostarter/common/sizes.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Ini Page Homee'));
+  }
+
+}
